15b095af250c511c13c352b6198a9cb9e6378be6
Max <max.hammer1998@googlemail.com>
2025-07-15 15:04:25 +0200

    fix: ensure resolution before and after interpolation is the same

--- src/interpolation.rs
+++ src/interpolation.rs
@@ -49,39 +49,15 @@ impl InterpolationAlgorithm for AverageAreaInterpolation {
         target_height: usize,
         pixel_format: PixelFormat,
     ) -> Result<Vec<u8>, InterpolationError> {
-        /*
-         * Uses average area downsampling interpolation
-         *
-         * Given kxk pixel block
-         * Block = [
-         * [R₁,G₁,B₁], [R₂,G₂,B₂], ..., [Rₖ,Gₖ,Bₖ],
-         * ...
-         * ]
-         *
-         * Compute average
-         * R_avg = (R₁ + R₂ + ... + Rₖ) / (k * k)
-         * G_avg = ...
-         * B_avg = ...
-         *
-         * P_avg = [R_avg, G_avg, B_avg]
-         */
-
         if target_height > src_height || target_width > src_width {
             return Err(InterpolationError::DownsampleTargetLargerThanSource(
                 format!(
-                    "Target Height {} > Source Height {}. For the downsampling to be reliable the target resolution has to be lower than the source resolution",
-                    target_height, src_height,
+                    "Target resolution ({}, {}) > Source resolution ({}, {})",
+                    target_width, target_height, src_width, src_height
                 ),
             ));
         }
-        if target_width > src_width {
-            return Err(InterpolationError::DownsampleTargetLargerThanSource(
-                format!(
-                    "Target Width {} > Source Width {}. For the downsampling to be reliable the target resolution has to be lower than the source resolution",
-                    target_width, src_width,
-                ),
-            ));
-        }
+
         let pixel_bytes: usize = pixel_format
             .pixel_bytes()
             .try_into()
@@ -92,27 +68,26 @@ impl InterpolationAlgorithm for AverageAreaInterpolation {
 
         let mut target_pixels: Vec<u8> =
             Vec::with_capacity(target_height * target_width * pixel_bytes);
+
         for block_y in 0..target_height {
             for block_x in 0..target_width {
-                let mut r_sum: usize = 0;
-                let mut g_sum: usize = 0;
-                let mut b_sum: usize = 0;
+                let mut sums = vec![0usize; pixel_bytes];
+
                 for y in 0..block_size_y {
                     for x in 0..block_size_x {
                         let pixel_x = block_x * block_size_x + x;
                         let pixel_y = block_y * block_size_y + y;
                         let idx = (pixel_y * src_width + pixel_x) * pixel_bytes;
-
-                        r_sum += src_pixels[idx] as usize;
-                        g_sum += src_pixels[idx + 1] as usize;
-                        b_sum += src_pixels[idx + 2] as usize;
+                        for channel in 0..pixel_bytes {
+                            sums[channel] += src_pixels[idx + channel] as usize;
+                        }
                     }
                 }
-                // compute the average
+
                 let count = block_size_x * block_size_y;
-                target_pixels.push((r_sum / &count) as u8);
-                target_pixels.push((g_sum / &count) as u8);
-                target_pixels.push((b_sum / &count) as u8);
+                for channel_sum in sums {
+                    target_pixels.push((channel_sum / count) as u8);
+                }
             }
         }
 
@@ -128,51 +103,42 @@ impl InterpolationAlgorithm for AverageAreaInterpolation {
         target_height: usize,
         pixel_format: PixelFormat,
     ) -> Result<Vec<u8>, InterpolationError> {
-        // Average area sampling is commonly only used for downsampling, but it can also be used for upsampling by using inverse mapping
-
         let pixel_bytes: usize = pixel_format
             .pixel_bytes()
             .try_into()
             .map_err(|_e: Infallible| InterpolationError::ImageMetadataResolve)?;
 
-        let mut target_pixels: Vec<u8> =
-            Vec::with_capacity(target_height * target_width * pixel_bytes);
+        let mut target_pixels = Vec::with_capacity(target_height * target_width * pixel_bytes);
         let scale_x = src_width as f64 / target_width as f64;
         let scale_y = src_height as f64 / target_height as f64;
+
         for y_target in 0..target_height {
             for x_target in 0..target_width {
-                let x_start: f64 = x_target as f64 * scale_x;
-                let x_end: f64 = (x_target + 1) as f64 * scale_x;
+                let x_start = (x_target as f64 * scale_x).floor() as usize;
+                let x_end = ((x_target + 1) as f64 * scale_x).ceil() as usize;
 
-                let y_start: f64 = y_target as f64 * scale_y;
-                let y_end = (y_target + 1) as f64 * scale_y;
+                let y_start = (y_target as f64 * scale_y).floor() as usize;
+                let y_end = ((y_target + 1) as f64 * scale_y).ceil() as usize;
 
-                // convert float bounds to specific pixels
-                let x0: usize = x_start.floor() as usize;
-                let x1: usize = x_end.ceil() as usize;
-                let y0: usize = y_start.floor() as usize;
-                let y1: usize = y_end.ceil() as usize;
-
-                let mut r_sum: usize = 0;
-                let mut g_sum: usize = 0;
-                let mut b_sum: usize = 0;
+                let mut sums = vec![0usize; pixel_bytes];
                 let mut count = 0;
-                for block_y in y0..y1 {
-                    for block_x in x0..x1 {
-                        let idx = (block_y * src_width + block_x) * pixel_bytes;
-                        r_sum += src_pixels[idx] as usize;
-                        g_sum += src_pixels[idx + 1] as usize;
-                        b_sum += src_pixels[idx + 2] as usize;
+
+                for y in y_start..y_end.min(src_height) {
+                    for x in x_start..x_end.min(src_width) {
+                        let idx = (y * src_width + x) * pixel_bytes;
+                        for c in 0..pixel_bytes {
+                            sums[c] += src_pixels[idx + c] as usize;
+                        }
                         count += 1;
                     }
                 }
 
-                // compute the average
-                target_pixels.push((r_sum / &count) as u8);
-                target_pixels.push((g_sum / &count) as u8);
-                target_pixels.push((b_sum / &count) as u8);
+                for sum in sums {
+                    target_pixels.push((sum / count) as u8);
+                }
             }
         }
+
         Ok(target_pixels)
     }
 }
@@ -187,64 +153,34 @@ impl InterpolationAlgorithm for NearestNeighborInterpolation {
         target_height: usize,
         pixel_format: PixelFormat,
     ) -> Result<Vec<u8>, InterpolationError> {
-        // original_height = input_image.height
-        // original_width = input_image.width
-        //
-        // # Create an empty image with new dimensions
-        // output_image = create_empty_image(new_width, new_height)
-        //
-        // # Calculate the scale factors
-        // x_scale = original_width / new_width
-        // y_scale = original_height / new_height
-        //
-        // for y_new from 0 to new_height - 1:
-        //     for x_new from 0 to new_width - 1:
-        //
-        // # Map coordinates in the output image back to the nearest input pixel
-        // x_old = floor(x_new * x_scale)
-        // y_old = floor(y_new * y_scale)
-        //
-        // # Assign the nearest pixel value
-        // output_image[y_new][x_new] = input_image[y_old][x_old]
-        // TODO: implement this
         if target_height > src_height || target_width > src_width {
             return Err(InterpolationError::DownsampleTargetLargerThanSource(
                 format!(
-                    "Target Height {} > Source Height {}. For the downsampling to be reliable the target resolution has to be lower than the source resolution",
-                    target_height, src_height,
+                    "Target resolution ({}, {}) > Source resolution ({}, {})",
+                    target_width, target_height, src_width, src_height
                 ),
             ));
         }
-        if target_width > src_width {
-            return Err(InterpolationError::DownsampleTargetLargerThanSource(
-                format!(
-                    "Target Width {} > Source Width {}. For the downsampling to be reliable the target resolution has to be lower than the source resolution",
-                    target_width, src_width,
-                ),
-            ));
-        }
-        let pixel_bytes: usize = pixel_format
-            .pixel_bytes()
-            .try_into()
-            .map_err(|_e: Infallible| InterpolationError::ImageMetadataResolve)?;
 
-        let mut target_pixels: Vec<u8> = vec![0u8; target_width * target_height * pixel_bytes];
-        let scale_x = src_width / target_width;
-        let scale_y = src_height / target_height;
+        let pixel_bytes = pixel_format.pixel_bytes() as usize;
+        let mut target_pixels = vec![0u8; target_width * target_height * pixel_bytes];
+
+        let scale_x = src_width as f64 / target_width as f64;
+        let scale_y = src_height as f64 / target_height as f64;
+
         for y in 0..target_height {
             for x in 0..target_width {
-                // clamp the coords so no overflow at the edges occurs.
-                let original_x = (((x * scale_x) as f64).floor() as usize).min(src_width - 1);
-                let original_y = (((y * scale_y) as f64).floor() as usize).min(src_height - 1);
+                let src_x = (x as f64 * scale_x).floor() as usize;
+                let src_y = (y as f64 * scale_y).floor() as usize;
 
-                let src_idx = (original_y * src_width + original_x) * pixel_bytes;
+                let src_idx = (src_y * src_width + src_x) * pixel_bytes;
                 let out_idx = (y * target_width + x) * pixel_bytes;
 
-                target_pixels[out_idx] = src_pixels[src_idx];
-                target_pixels[out_idx + 1] = src_pixels[src_idx + 1];
-                target_pixels[out_idx + 2] = src_pixels[src_idx + 2];
+                target_pixels[out_idx..out_idx + pixel_bytes]
+                    .copy_from_slice(&src_pixels[src_idx..src_idx + pixel_bytes]);
             }
         }
+
         Ok(target_pixels)
     }
 
@@ -257,33 +193,30 @@ impl InterpolationAlgorithm for NearestNeighborInterpolation {
         target_height: usize,
         pixel_format: PixelFormat,
     ) -> Result<Vec<u8>, InterpolationError> {
-        let pixel_bytes: usize = pixel_format
-            .pixel_bytes()
-            .try_into()
-            .map_err(|_e: Infallible| InterpolationError::ImageMetadataResolve)?;
-        let mut target_pixels: Vec<u8> = vec![0u8; target_width * target_height * pixel_bytes];
+        let pixel_bytes = pixel_format.pixel_bytes() as usize;
+        let mut target_pixels = vec![0u8; target_width * target_height * pixel_bytes];
 
         if target_pixels.len() <= src_pixels.len() {
             return Err(InterpolationError::UpsampleSourceLargerThanTarget(format!(
-                "The length of the source pixel vector is {} and the length of the target pixel vector is {}. For a reliable upsampling the target pixel vector needs to be greater.",
+                "Source pixel vec is {}, target vec is {}",
                 src_pixels.len(),
-                target_pixels.len(),
+                target_pixels.len()
             )));
         }
-        let scale_x = target_width / src_width;
-        let scale_y = target_height / src_height;
+
+        let scale_x = src_width as f64 / target_width as f64;
+        let scale_y = src_height as f64 / target_height as f64;
+
         for y in 0..target_height {
             for x in 0..target_width {
-                // clamp the coords so no overflow at the edges occurs.
-                let original_x = (((x / scale_x) as f64).floor() as usize).min(src_width - 1);
-                let original_y = (((y / scale_y) as f64).floor() as usize).min(src_height - 1);
+                let src_x = (x as f64 * scale_x).floor() as usize;
+                let src_y = (y as f64 * scale_y).floor() as usize;
 
-                let src_idx = (original_y * src_width + original_x) * pixel_bytes;
+                let src_idx = (src_y * src_width + src_x) * pixel_bytes;
                 let out_idx = (y * target_width + x) * pixel_bytes;
 
-                target_pixels[out_idx] = src_pixels[src_idx];
-                target_pixels[out_idx + 1] = src_pixels[src_idx + 1];
-                target_pixels[out_idx + 2] = src_pixels[src_idx + 2];
+                target_pixels[out_idx..out_idx + pixel_bytes]
+                    .copy_from_slice(&src_pixels[src_idx..src_idx + pixel_bytes]);
             }
         }
 
@@ -315,4 +248,59 @@ pub fn run_interpolation(
         src_height.into(),
         metadata.pixel_format,
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{NearestNeighborInterpolation, run_interpolation};
+    use crate::interpolation::AverageAreaInterpolation;
+    use jpeg_decoder::{CodingProcess, ImageInfo, PixelFormat};
+
+    #[test]
+    fn test_nearest_neighbor_interpolation() {
+        let width = 4;
+        let height = 4;
+        let pixel_format = 3;
+        let mock_pixels: Vec<u8> = vec![128u8; width * height * pixel_format];
+        let original_pixels = mock_pixels.clone();
+        let metadata = ImageInfo {
+            width: width as u16,
+            height: height as u16,
+            pixel_format: PixelFormat::RGB24,
+            coding_process: CodingProcess::DctSequential,
+        };
+        let target_resolution = 2;
+        let result_pixels = run_interpolation(
+            &NearestNeighborInterpolation,
+            mock_pixels,
+            target_resolution,
+            metadata,
+        )
+        .unwrap();
+        assert_eq!(result_pixels.len(), original_pixels.len());
+    }
+
+    #[test]
+    fn test_average_area_interpolation() {
+        let width = 4;
+        let height = 4;
+        let pixel_format = 3;
+        let mock_pixels: Vec<u8> = vec![128u8; width * height * pixel_format];
+        let original_pixels = mock_pixels.clone();
+        let metadata = ImageInfo {
+            width: width as u16,
+            height: height as u16,
+            pixel_format: PixelFormat::RGB24,
+            coding_process: CodingProcess::DctSequential,
+        };
+        let target_resolution = 2;
+        let result_pixels = run_interpolation(
+            &AverageAreaInterpolation,
+            mock_pixels,
+            target_resolution,
+            metadata,
+        )
+        .unwrap();
+        assert_eq!(result_pixels.len(), original_pixels.len());
+    }
 }
